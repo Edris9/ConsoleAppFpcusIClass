746165bcf669e75ab3fb4ab8b6dfe66129b29cc2
Edris9 <Edriskohestani1010@gmail.com>
2025-10-08 15:01:20 +0200

    its works

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace ConsoleAppFpcusIClass
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace ConsoleAppFpcusIClass
 {
     internal class Program
+    
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Lön Kalkylator: ");
+
+            SalaryCalculate salaryCalculate = new SalaryCalculate();
+            
+            Console.WriteLine("skriv ditt namn först: ");
+            String Name = Console.ReadLine();
+            //
+            Console.WriteLine("Hej " + Name);
+            salaryCalculate.Calculate();
         }
     }
 }
---
+++ SalaryCalculate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFpcusIClass
+{
+    internal class SalaryCalculate
+    {
+        
+        int salary;
+        double Tax;
+
+        public void Calculate()
+        {
+            Console.WriteLine("Skriv din lön innan skatt: ");
+            salary = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Skrive din kommuns skatt: ");
+            Tax = Convert.ToDouble(Console.ReadLine());
+
+
+            double skatt = salary * (Tax / 100);
+
+            Console.WriteLine("Din betalade skatt är : "+ skatt);
+
+            double lön = salary - skatt;
+
+            Console.WriteLine("Du har ett lön på " + lön);
+
+
+        }
+
+        
+    }
+
+}
