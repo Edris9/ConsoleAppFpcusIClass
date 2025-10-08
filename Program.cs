a0e16d16f675aea57cc7d9169ac475e249257f40
Edris9 <Edriskohestani1010@gmail.com>
2025-10-08 12:43:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppFpcusIClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
